3b19e0fba0680a641d75800a537a86fcaff80ae2
Michelle Jowers <michelle.jowers@icloud.com>
2021-06-02 11:20:21 -0400

    init commit

---
+++ ch01_Test/src/TestApp.java
@@ -0,0 +1,10 @@
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello There!");
+		System.out.println("Java day 1!!!!!!");
+	}
+
+}
