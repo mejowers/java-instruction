155e391417869fc699c7725d9ee228af0d4c8ba5
Michelle Jowers <michelle.jowers@icloud.com>
2021-06-08 19:08:08 -0400

    add and update files

--- ch07_prj5_Bmdb/src/BmdbConsoleApp.java
+++ ch07_prj5_Bmdb/src/BmdbConsoleApp.java
@@ -3,8 +3,20 @@ public class BmdbConsoleApp {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Bmdb App!");
+		System.out.println();
 		
-		String movie = 
+		int id = Console.getInt("Id? ");
+		String title = Console.getString("Title? ");
+		String rating = Console.getString("Rating? ");
+		int year = Console.getInt("Year? ", 1800, 9999);
+		String director = Console.getString("Director? ");
+		System.out.println();
+		
+		
+		Movie m = new Movie(id, title, rating, year, director);
+		
+		System.out.println("Product Summary: "+m);
+		System.out.println();
 
 	}
 	
