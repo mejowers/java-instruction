046cf403333d6929dc7e784b3e445d93fe55d727
Michelle Jowers <michelle.jowers@icloud.com>
2021-06-12 12:35:28 -0400

    add and update files

--- ch04_ex3_GuessNumber/src/GuessNumberApp.java
+++ ch04_ex3_GuessNumber/src/GuessNumberApp.java
@@ -16,27 +16,26 @@ public class GuessNumberApp {
 
         @SuppressWarnings("resource")
         Scanner sc = new Scanner(System.in);            
-        int count = 1;
-        while (true) {
+        int count = 0;
+        int guess = 0;
+        
+        while (guess != number) {
             System.out.print("Your guess: ");
-            int guess = sc.nextInt();
+            guess = sc.nextInt();
             
             if (guess < 1 || guess > LIMIT) {
                 System.out.println("Invalid guess. Try again.");
-                continue;
-            }
-            
-            if (guess < number) {
+                count --;
+            } else if (guess < number) {
                 System.out.println("Too low.");
             } else if (guess > number) {
                 System.out.println("Too high.");
-            } else {
-                System.out.println("You guessed it in " + 
-                                   count + " tries.\n");
-                break;
-            }            
+            }
+                    
             count++;
         }
+        System.out.println("You guessed it in " + 
+                count +" tries.\n");
         System.out.println("Bye!");
-    }   
+    } 
 }
