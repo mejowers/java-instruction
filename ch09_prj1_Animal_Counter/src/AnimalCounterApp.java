44554fa75ff9457fe4c08aafb641a4cd736a5c7f
Michelle Jowers <michelle.jowers@icloud.com>
2021-06-13 17:45:17 -0400

    add and update files

---
+++ ch09_prj1_Animal_Counter/src/AnimalCounterApp.java
@@ -0,0 +1,23 @@
+
+public class AnimalCounterApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the animal counting app!");
+		System.out.println();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("Good Bye!");
+		
+
+	}
+
+}
