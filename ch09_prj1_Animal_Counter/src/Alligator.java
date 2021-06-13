762e5b619c56195ecc09d80272053c765508d85a
Michelle Jowers <michelle.jowers@icloud.com>
2021-06-13 16:21:23 -0400

    add and update files

--- ch09_prj1_Animal_Counter/src/Alligator.java
+++ ch09_prj1_Animal_Counter/src/Alligator.java
@@ -1,31 +1,4 @@
 
-public abstract class Alligator extends Animal implements Counter {
+public class Alligator extends Animal {
 
-	private int alligatorCount;
-
-	public Alligator(int count, int alligatorCount) {
-		super(count);
-		this.alligatorCount = alligatorCount;
-	}
-
-	public Alligator(int count) {
-		super(count);
-	}
-
-	public int getAlligatorCount() {
-		return alligatorCount;
-	}
-
-	public void setAlligatorCount(int alligatorCount) {
-		this.alligatorCount = alligatorCount;
-	}
-
-	@Override
-	public String toString() {
-		return "Alligator [alligatorCount=" + alligatorCount + "]";
-	}
-	
-	
-	
-	
 }
--- ch09_prj1_Animal_Counter/src/Animal.java
+++ ch09_prj1_Animal_Counter/src/Animal.java
@@ -1,30 +1,46 @@
 
-public abstract class Animal implements Counter{
+public class Animal implements Counter {
 	
-private int count;
-
-public Animal(int count) {
-	super();
-	this.count = count;
-}
-
-public Animal() {
-	super();
-}
-
-public int getCount() {
-	return count;
-}
-
-public void setCount(int count) {
-	this.count = count;
-}
-
-@Override
-public String toString() {
-	return "Animal [count=" + count + "]";
-}
-
-
+	private int count;
 	
+	public Animal(int count) {
+		super();
+		this.count = count;
+	}
+	
+
+	public Animal() {
+		super();
+	}
+
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+
+	@Override
+	public void incrementCount() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resetCount() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getCountString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
