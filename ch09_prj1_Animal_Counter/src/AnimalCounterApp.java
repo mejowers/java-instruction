8df0361e7c70403455b5dcb26fd7f5d698e3f2be
Michelle Jowers <michelle.jowers@icloud.com>
2021-06-13 21:55:21 -0400

    add and update files

--- ch09_prj1_Animal_Counter/src/AnimalCounterApp.java
+++ ch09_prj1_Animal_Counter/src/AnimalCounterApp.java
@@ -2,30 +2,21 @@
 public class AnimalCounterApp {
 
 	public static void main(String[] args) {
-		
 		System.out.println("Welcome to the animal counting app!");
 		System.out.println();
-		
-		int maxCount = 3;
-		
+	
 		
 		public static void count(Countable c, int maxCount) {
-		count++;
-		System.out.println(+Alligator);
-		
-		
-		}
-		
-		
-		
-		
-		
-		
-		
-		
+			c=Alligators;
+			maxCount=3;
+			count=0;
+			for (int count = 0, count <= maxCount, count++) {
+				return System.out.println(+Alligator)
+			}
+		}	
 		System.out.println("Good Bye!");
 		
 
 	}
-	
+
 }
