befe7d18b9751a5efb80049f9d0ce63507123bfe
Michelle Jowers <michelle.jowers@icloud.com>
2021-06-14 22:10:11 -0400

    add and update files

--- ch04_prj1_TOP_redo/src/ch04_prj1_TOP_redo/TableOfPowers.java
+++ ch04_prj1_TOP_redo/src/ch04_prj1_TOP_redo/TableOfPowers.java
@@ -6,10 +6,12 @@ public class TableOfPowers {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Squares and Cubes table");
+		System.out.println();
 
 		String choice = "y";
 		while (choice.equalsIgnoreCase("y")) {
 			int number = Console.getInt("Enter an integer: ");
+			System.out.println();
 
 			System.out.println("Number Squared Cubed");
 			System.out.println("====== ======= =====");
@@ -17,12 +19,13 @@ public class TableOfPowers {
 			for (int i = 1; i <= number; i++) {
 				int squared = (i * i);
 				int cubed = (i * i * i);
-				System.out.println(i + "      " + squared + "       " + cubed);
+				System.out.println(i + "\t" + squared + "\t" + cubed);
 
 			}
+			System.out.println();
 			choice = Console.getLine("Do you want to continue: (y/n)");
 		}
-
+		System.out.println();
 		System.out.println("Good Bye");
 
 	}
---
+++ ch04_prj2_Fact_Calc_redo/src/ch04_prj2_Fact_Calc_redo/FactorialCalculatorAppRedo.java
@@ -0,0 +1,30 @@
+package ch04_prj2_Fact_Calc_redo;
+
+import com.util.Console;
+
+public class FactorialCalculatorAppRedo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Factorial Calculator");
+		System.out.println();
+		
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			int nbr  = Console.getInt("Enter an integer that is greater than 0 and"
+					+ " less than 10: ", 0, 10);
+			
+			long factorial = 1;
+			for (int i = 1; i<=nbr; i++) {
+			factorial *= i; 
+			Console.getString("The factorial for "+nbr+" is "+factorial+"!");
+			}
+			
+	
+		Console.getLine("Continue? (y/n)");
+		
+		}
+		System.out.println();		
+		System.out.println("Good Bye!");
+
+	}
+}
