8e6c50b35aded369b1ee52f7af53788cbf64a9e1
Michelle Jowers <michelle.jowers@icloud.com>
2021-06-20 20:53:59 -0400

    update files

--- ch14_prj1_Reservation_Calculator/src/business/Reservation.java
+++ ch14_prj1_Reservation_Calculator/src/business/Reservation.java
@@ -1,6 +1,8 @@
 package business;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 import com.util.Console;
 
--- ch14_prj1_Reservation_Calculator/src/ui/ReservationCalculatorApp.java
+++ ch14_prj1_Reservation_Calculator/src/ui/ReservationCalculatorApp.java
@@ -1,10 +1,13 @@
 package ui;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 import com.util.Console;
 
-public class ReservationCalculatorApp {
+import business.Reservation;
+
+public class ReservationCalculatorApp extends Reservation {
 
 	public static void main(String[] args) {
 		System.out.println("****Reservation Calculator*****");
@@ -12,6 +15,19 @@ public class ReservationCalculatorApp {
 		
 		String choice = "y";
 		while (choice.equalsIgnoreCase("y")) {
+			System.out.println("Enter Arrival Information:");
+			int arrivalYear = Console.getInt("Year: ");
+			int arrivalMonth = Console.getInt("Month: ");
+			int arrivalDay = Console.getInt("Day: ");
+			
+			System.out.println();
+			System.out.println("Enter Departure Information:");
+			int departureYear = Console.getInt("Year: ");
+			int departureMonth = Console.getInt("Month: ");
+			int departureDay = Console.getInt("Day: ");
+			
+			
+			
 			
 			
 	   
@@ -22,7 +38,8 @@ public class ReservationCalculatorApp {
 		        
 		        
 		        
-		choice = Console.getString("Continue: (y/n)?", "y", "n");     
+		choice = Console.getString("Continue: (y/n)?", "y", "n"); 
+		System.out.println();
 		        
 		}
 		
